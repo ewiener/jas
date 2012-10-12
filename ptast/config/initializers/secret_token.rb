d706f1a4a36ba6ba32f05c449ec0ef4c48348c16
saasbook <saasbook@saasbook.(none)>
2012-10-12 16:33:33 -0700

    Removed the ptast directory from the repository since it is now at the root

--- ptast/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Ptast::Application.config.secret_token = '5efd44fffcf6d2f597dd8316194d58309650a620519a7dd974c2973c7108a9821075d3f0fd43794a9660533ce43c4328586f8dd7448c3ef742be083d1e5469ca'

05750b23c2d074893da47022dac018ec13ed7cc1
Eric <ehorton@berkeley.edu>
2012-10-19 19:27:44 -0700

    Added Cucumber test for login and skeleton files for rspec

---
+++ spec/controllers/semesters_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe SemestersController do
+  describe 'Create a new Session' do
+    it 'When I go to the home page, and I click on "Create New Session", it should be on the New Session page' do

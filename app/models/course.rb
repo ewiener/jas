db1e5aaa07d3331653e1504bd68358bd8ea9265a
TheUnknownUniverse <saasbook@saasbook.(none)>
2012-10-20 04:03:44 -0700

    Added a bunch of validation checks to the Semester model and to the Course model.  Cleaned up whitespace in the semesters_controller.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,20 +1,167 @@
 # PTA Course held within a specific Semester.
-# PTA Course held within a specific Semester.
-# PTA Course held within a specific Semester.
-# PTA Course held within a specific Semester.
-# PTA Course held within a specific Semester:
-# * asdfasdf
-# * asdfasdfasdf
-# * asdfasdfa
 class Course < ActiveRecord::Base
+  require 'time'
   serialize :days_of_week
-  attr_accessor :name, :description, :days_of_week, :number_of_classes, :start_time, :end_time, :class_min, :class_max, :grade_range, :fee_per_meeting, :fee_for_additional_materials, :total_fee
+  TIME_TYPE = [0, 1, 2] # AM, PM, 24HR
+  HOUR12 = Array(1..12)
+  HOUR24 = Array(0..23)
+  MINUTE = Array(0..59)
+  attr_accessor :name, :description, :days_of_week, :number_of_classes, :start_time_hour, :start_time_minute, :start_time_type, :end_time_hour, :end_time_minute, :end_time_type, :class_min, :class_max, :grade_range, :fee_per_meeting, :fee_for_additional_materials, :total_fee
 
   #has location through teacher
-
 
   belongs_to :session
   has_many :ptainstructors
   has_many :students
 
+  validate :name_is_valid
+  validates :description, :presence => true
+  validate :days_of_week_is_valid
+  validate :number_of_classes_is_valid
+  validate :start_time_hour_is_valid
+  validate :start_time_minute_is_valid
+  validate :start_time_type_is_valid
+  validate :end_time_hour_is_valid
+  validate :end_time_minute_is_valid
+  validate :end_time_type_is_valid
+  validate :class_min_valid
+  validate :class_max_valid
+  validate :grade_range_valid
+  validate :fee_per_meeting_is_valid
+  validate :fee_for_additional_materials_is_valid
+  validate :total_fee_is_valid
+
+  private
+  def name_is_valid
+    errors.add(:name,'Invalid empty string for name.') unless name_is_valid?
+  end
+
+  def name_is_valid?
+    return self.name.length > 0
+  end
+
+  private
+  def days_of_week_is_valid
+
+  end
+
+  def days_of_week_is_valid?
+
+  end
+
+  private
+  def number_of_classes_is_valid
+    errors.add(:number_of_classes, 'Invalid number of classes') unless number_of_classes_is_valid?
+  end
+
+  def number_of_classes_is_valid?
+    return self.number_of_classes > 0
+  end
+
+  private
+  def start_time_hour_is_valid
+    errors.add(:start_time_hour, 'Invalid starting time hour.') unless start_time_hour_is_valid?
+  end
+
+  def start_time_hour_is_valid?
+      return hour_valid?(self.start_time_hour)
+  end
+
+  private
+  def start_time_minute_is_valid
+    errors.add(:start_time_minute,'Invalid starting time minute.') unless start_time_minute_is_valid?
+  end
+
+  def start_time_minute_is_valid?
+    return minute_valid?(self.end_time_minute)
+  end
+
+  private
+  def end_time_hour_is_valid
+    errors.add(:end_time_hour, 'Invalid ending time hour.') unless end_time_hour_is_valid?
+  end
+
+  def end_time_hour_minute_is_valid?
+    return hour_valid?(self.end_time_hour)
+  end
+
+  private
+  def end_time_minute_is_valid
+    errors.add(:end_time_minute, 'Invalid ending time minute.') unless end_time_minute_is_valid?
+  end
+
+  def end_time_minute_is_valid?
+    return minute_valid?(self.end_time_hour)
+  end
+
+  private
+  def hour_valid?(hour)
+    case hour
+    when 0..1
+      return HOUR12.include? hour
+    when 2
+      return HOUR24.include? hour
+    end
+  end
+
+  private
+  def minute_valid?(minute)
+    return MINUTE.include?(minute)
+  end
+
+  private
+  def start_time_type_is_valid
+    errors.add(:start_time_type, 'Invalid start time type.  Should be AM, PM, or 24HR.') unless start_time_type_is_valid?
+  end
+
+  def start_time_type_is_valid?
+    return TIME_TYPE.include? self.start_time_type
+  end
+
+  private
+  def end_time_type_is_valid
+    errors.add(:end_time_type,'Invalid end time type.  Should be AM, PM, or 24HR.') unless end_time_type_is_valid?
+  end
+
+  def end_time_type_is_valid?
+    return TIME_TYPE.include? self.end_time_type
+  end
+
+  private
+  def class_min_is_valid
+    errors.add(:class_min,'Invalid class min value.') unless class_min_is_valid?
+  end
+
+  def class_min_is_valid?
+    return self.class_min > 0 and self.class_max >= self.class_min
+  end
+
+  private
+  def class_max_is_valid
+    errors.add(:class_max,'Invalid class max value.') unless class_max_is_valid?
+  end
+
+  def class_max_is_valid?
+    return self.class_max > 0 and self.class_max >= self.class_min
+  end
+
+  #TODO: Need to define :grade_range_is_valid
+
+  private
+  def fee_for_additional_materials_is_valid
+    errors.add(:fee_for_additional_materials, 'The fee for additional materials is invalid.') unless fee_for_additional_materials_is_valid?
+  end
+
+  def fee_for_additional_materials_is_valid?
+    return self.fee_for_addtional_materials >= 0
+  end
+
+  private
+  def total_fee_is_valid
+    errors.add(:total_fee,'The total fee is invalid.') unless total_fee_is_valid?
+  end
+
+  def total_fee_is_valid?
+    return self.total_fee >= 0
+  end
 end
--- app/models/semester.rb
+++ app/models/semester.rb
@@ -5,61 +5,88 @@ class Semester < ActiveRecord::Base
   serialize :dates_with_no_classes
   attr_accessor :name, :start_date, :end_date, :dates_with_no_classes, :lottery_deadline, :registration_deadline
 
-  validates :name, :presence => true
+  validate :name_is_valid
 
-  validates :valid_start_date
-  validates :valid_end_date
-  validates :start_date_before_end_date
+  validate :valid_start_date
+  validate :valid_end_date
+  validate :start_date_before_end_date
 
-  validates :dates_with_no_classes_in_session
+  validate :dates_with_no_classes_in_session
 
-  validates :valid_lottery_date
-  validates :valid_registration_date
+  validate :valid_lottery_date
+  validate :valid_registration_date
 
   has_many :courses
   has_many :students, :through => :courses
   has_many :ptainstructors, :through => :courses
   has_many :teachers, :through => :courses
 
-  # Verifies that the start date is not the end date and that it comes before the end date.
+  private
+  # Used by validation check to verify that the name is valid
+  def name_is_valid
+    errors.add(:name,"Invalid empty string for name.") unless name_is_valid?
+  end
+
+  # Verifies that the name is valid
+  def name_is_valid?
+    return self.name.length > 0
+  end
+
+  private
+  # Verifies that the start date comes before the end date
   def start_date_before_end_date
-    date_start = Date.parse(self.start_date)
-    date_end = Date.parse(self.end_date)
+    date_start = USDateParse(self.start_date)
+    date_end = USDateParse(self.end_date)
     if date_start >= date_end
       errors.add(:start_date, "Start date must be before end date.")
     end
   end
 
-  # Verifies that the start date can be parsed
+  private
+  # Used by validation check to verify that the start date can be parsed
   def valid_start_date
+      errors.add(:start_date, 'The start date cannot be parsed.') unless start_date_is_valid?
+  end
+
+  # Verifies that the start date can be parsed
+  def start_date_is_valid?
     begin
-      Date.parse(self.start_date)
+      USDateParse(self.start_date)
     rescue
-      errors.add(:start_date, 'The start date cannot be parsed.')
+      return false
     end
+    return true
+  end
+
+  private
+  # Used by validation check to verify that the end date can be parsed
+  def valid_end_date
+    errors.add(:end_date, 'The end date cannot be parsed.') unless end_date_is_valid?
   end
 
   # Verifies that the end date can be parsed
-  def valid_end_date
+  def end_date_is_valid?
     begin
-      Date.parse(self.end_date)
+       USDateParse(self.end_date)
     rescue
-      errors.add(:end_date, 'The end date cannot be parsed.')
+      return false
     end
+    return true
   end
 
+  private
   # Verifies that the dates that there are no classes are within the session start date and session end date.
   def dates_with_no_classes_in_session
     begin
-      begin_parse = Date.parse(self.start_date)
-      end_parse = Date.parse(self.end_date)
+      begin_parse = USDateParse(self.start_date)
+      end_parse = USDateParse(self.end_date)
     rescue
       errors.add(:dates_with_no_classes, 'Could not verify that the dates with no classes fall within the session because the start and/or end dates are not parseable.')
       return
     end
     self.dates_with_no_classes.each do |date|
       begin
-        date_parsed = Date.parse(date)
+        date_parsed = USDateParse(date)
       rescue
         errors.add(:dates_with_no_classes, 'The date, "#{date}", could not be parsed.')
       end
@@ -69,25 +96,42 @@ class Semester < ActiveRecord::Base
     end
   end
 
+  private
   # Verifies that the lottery date can be parsed
   def valid_lottery_date
     begin
-      Date.parse(:lottery_deadline)
+      USDateParse(:lottery_deadline)
     rescue
       errors.add(:lottery_deadline, 'The lottery deadline could not be parsed.')
     end
   end
 
+  private
+
+  # Used by the validation check to make sure that the registration date can be parsed.
   def valid_registration_date
-    begin
-      Date.parse(:registration_deadline)
-    rescue
-      errors.add(:registration_deadline, 'The registration deadline could not be parsed.')
-    end
+    errors.add(:registration_deadline, 'The registration deadline could not be parsed.')
   end
 
+  # Verifies that the registration date can be parsed
+  def registration_date_is_valid?
+    begin
+         USDateParse(:registration_deadline)
+    rescue
+      return false
+    end
+    return true
+  end
+
+  private
+  # Verifies that the date can be parsed
+  # It checks that the date is between January 1, 2000 and January 1, 2100
   def USDateParse(date)
     date = Date.strptime(date,'%d/%m/%Y')
+    year_2000 = Date.new(2000,1,1)
+    year_2100 = Date.new(2100,1,1)
+    if date <= year_2000 or date >= year_2100
+      raise "Invalid Date"
+    end
   end
-
 end

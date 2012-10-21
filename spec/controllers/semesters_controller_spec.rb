c9a60b6d744161683005aee3dd39309ba742281c
Eric <ehorton@berkeley.edu>
2012-10-20 18:47:22 -0700

    modified 3 rspecs

--- spec/controllers/semesters_controller_spec.rb
+++ spec/controllers/semesters_controller_spec.rb
@@ -6,6 +6,21 @@ describe SemestersController do
     end
 
     it 'And I fill in "Session Name" with "Fall 2012" and I fill in "Start Date" with "September 3" and I fill in "End Date" with "October 1" and I fill in "Lottery Deadline" with "September 10" and I fill in "Registration Deadline" with "September 17" and I click "One Day" in "Days Wont Meet" and I click "September 24" and I fill in "Registration Fee" with "25" and I click "Create", it should save the session' do
-end
+    end
 
+    describe 'Create a new Session with "Start Date" after "End Date"' do
+      it 'And I fill in "Session Name" with "Fall 2012" and I fill in "Start Date" with "October 1" and I fill in "End Date" with "September 3" and I fill in "Lottery Deadline" with "September 10" and I fill in "Registration Deadline" with "September 17" and I click "One Day" in "Days Wont Meet" and I click "September 24" and I fill in "Registration Fee" with "25" and I click "Create", it should not save the session' do
+      end
+
+    end
+
+    describe 'Create a new Session with "Days Class Wont Meet" in which the span of days is not possible' do
+      it 'And I fill in "Session Name" with "Fall 2012" and I fill in "Start Date" with "September 3" and I fill in "End Date" with "October 1" and I fill in "Lottery Deadline" with "September 10" and I fill in "Registration Deadline" with "September 17" and I click "Span of Days" in "Days Wont Meet" and I click "September 24" and "September 23" and I fill in "Registration Fee" with "25" and I click "Create", it should not save the session' do
+      end
+    end
+
+    describe 'Create a new Session without "Registration Fee" filled' do
+      it 'And I fill in "Session Name" with "Fall 2012" and I fill in "Start Date" with "September 3" and I fill in "End Date" with "October 1" and I fill in "Lottery Deadline" with "September 10" and I fill in "Registration Deadline" with "September 17" and I click "One Day" in "Days Wont Meet" and I click "September 24" and I click "Create", it should not save the session' do
+      end
+    end
 

3c5ea1c5f05936395e6cc69bca6e6b26cc1057dc
Eddie <eddieberklee@gmail.com>
2012-10-24 06:16:30 -0700

    added partial errors that i had forgotten to add, also included a seed file for adding 2 semesters so far

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Semester.create(name: "Fall 2012",
+                start_date: "08/22/2012",
+                end_date: "12/24/2012",
+                lottery_deadline: "08/22/2012",
+                registration_deadline: "08/22/2012",
+                fee: 100.0)
+Semester.create(name: "Spring 2012",
+                start_date: "01/22/2012",
+                end_date: "05/13/2012",
+                lottery_deadline: "01/22/2012",
+                registration_deadline: "01/22/2012",
+                fee: 200.0)
+
+
+
+

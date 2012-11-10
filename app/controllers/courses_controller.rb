d0b9b94c0f33ecb6f9632782a717bba1e8ca49ae
TheUnknownUniverse <saasbook@saasbook.(none)>
2012-11-10 15:46:08 -0800

    Fixed an issue where the new controller method was failing because it couldn't call find_all_by_semester_id.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -21,8 +21,8 @@ class CoursesController  < ApplicationController
   def new
     @semester = Semester.find_by_id params[:semester_id]
     return unless semester_is_valid(@semester)
-    @ptainstructors = Ptainstructor.find_all_by_semester_id @semester
-    @teachers = Teacher.find_all_by_semester_id @semester
+    @ptainstructors = Ptainstructor.where( :semester_id => @semester )
+    @teachers = Teacher.where( :semester_id => @semester )
     if flash.key? :course
       @course = flash[:course]
       render 'new'

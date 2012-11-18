2bec3b7efcbbba5e4f334c31c41c5181aae95daf
TheUnknownUniverse <saasbook@saasbook.(none)>
2012-11-18 02:55:01 -0800

    Added import to the site.  The functionality for importing works, but the views aren't there.  Tested via the rails console though and it worked.

--- db/schema.rb
+++ db/schema.rb
@@ -63,15 +63,17 @@ ActiveRecord::Schema.define(:version => 20121118022550) do
   end
 
   create_table "students", :force => true do |t|
-    t.text "first_name"
-    t.text "last_name"
-    t.text "grade"
-    t.text "student_phone"
-    t.text "parent_phone"
-    t.text "parent_phone2"
-    t.text "parent_name"
-    t.text "parent_email"
-    t.text "health_alert"
+    t.text    "first_name"
+    t.text    "last_name"
+    t.text    "grade"
+    t.text    "student_phone"
+    t.text    "parent_phone"
+    t.text    "parent_phone2"
+    t.text    "parent_name"
+    t.text    "parent_email"
+    t.text    "health_alert"
+    t.integer "semester_id"
+    t.integer "teacher_id"
   end
 
   create_table "teachers", :force => true do |t|

74c7ed720b5984432682b8929367cd106c2f9c7e
TheUnknownUniverse <saasbook@saasbook.(none)>
2012-11-24 12:40:53 -0800

    Undid Eddie's change to the gemfile that broke rake and forced upgrade to rake version 10.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,50 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'ostruct'
-
-semesters = []
-semesters.push(OpenStruct.new({
-	:name => 'Fall 2012',
-	:start_date => '08/22/2012',
-	:end_date => '10/24/2012',
-	:lottery_deadline => '08/22/2012',
-	:registration_deadline => '08/22/2012',
-	:fee => 1000,
-}))
-semesters.push(OpenStruct.new({
-	:name => 'Spring 2012',
-	:start_date => '01/28/2012',
-	:end_date => '03/14/2012',
-	:lottery_deadline => '08/22/2012',
-	:registration_deadline => '08/22/2012',
-	:fee => 2000,
-}))
-semesters.push(OpenStruct.new({
-	:name => 'Winter 2012',
-	:start_date => '11/28/2012',
-	:end_date => '12/14/2012',
-	:lottery_deadline => '08/22/2012',
-	:registration_deadline => '08/22/2012',
-	:fee => 2000,
-}))
-semesters.each do |isemester|
+semesters = ['Fall 2012', 'Spring 2012', 'Winter 2012']
+semesters.each do |my_semester|
   # Adding Semesters
   Semester.create(
-    name: isemester.name,
-    start_date: isemester.start_date,
-		end_date: isemester.end_date,
-    lottery_deadline: isemester.lottery_deadline,
-    registration_deadline: isemester.registration_deadline,
-    fee: isemester.fee
+    name: my_semester,
+    start_date: "08/22/2012",
+    end_date: "12/24/2012",
+    lottery_deadline: "08/22/2012",
+    registration_deadline: "08/22/2012",
+    fee: 1000.0
   )
 
   # Adding PTA Instructors
-  semester = Semester.find_by_name(isemester.name)
+  semester = Semester.find_by_name(my_semester)
   number_of_ptainstructors = 3
   number_of_ptainstructors.times do |t|
     semester.ptainstructors.create(
-      name: "#{isemester.name} PTA Instructor #{t+1}",
+      name: "#{my_semester} PTA Instructor #{t+1}",
       email: "ptainstructor#{t+1}@gmail.com",
       phone: "1234567890",
       address: "An address somewhere",
@@ -58,63 +32,55 @@ semesters.each do |isemester|
 
   # Adding Teachers
   grades = 'K,1,2,3,4,5'.split(',')
-
+  randomGrade = grades[rand(grades.length)]
   number_of_teachers = 3
-	teacher_names = ['Takashi Murakami', 'Kanye West', 'Jermaine Cole']
-
   number_of_teachers.times do |t|
-  	randomGrade = grades[rand(grades.length)]
     semester.teachers.create(
-      name: teacher_names[t],
+      name: "#{my_semester} Teacher #{t+1}",
       grade: "#{randomGrade}",
       classroom: "A classroom somewhere",
     )
   end
 
-  # Adding Courses
-  grades = 'K,1,2,3,4,5'.split(',')
-  ps = []
-  ts = []
-  Ptainstructor.all.each do |ptainstructa|
-    ps.push(ptainstructa.id)
-  end
-  Teacher.all.each do |teacha|
-    ts.push(teacha.id)
-  end
-  number_of_courses = 3
-  number_of_courses.times do |t|
-    randomGrade = grades[rand(grades.length)]
-    randomMin = rand(50)
-    randomMax = rand(50) + 50
-    random = rand(100)
-    ptainstructorId = ps[rand(ps.length)]
-    teacherId = ts[rand(ts.length)]
-    ptainstructor = Ptainstructor.find_by_id(ptainstructorId)
-    teacher = Teacher.find_by_id(teacherId)
-
-    semester.courses.create(
-      name: "#{isemester.name} Course #{t+1}",
-      description: "#{isemester.name} course ##{t+1}",
-      sunday: true,
-      monday: true,
-      tuesday: true,
-      wednesday: true,
-      thursday: true,
-      friday: true,
-      saturday: true,
-      start_time: "#{t+1}:00pm",
-      end_time: "#{t+1}:00pm",
-      grade_range: "#{randomGrade}",
-      class_min: randomMin,
-      class_max: randomMax,
-      number_of_classes: randomMin,
-      fee_per_meeting: random,
-      fee_for_additional_materials: random,
-      total_fee: random,
-      ptainstructor: ptainstructor,
-      teacher: teacher,
-    )
-  end
+#  # Adding Courses
+#  grades = 'K,1,2,3,4,5'.split(',')
+#  ps = []
+#  ts = []
+#  Ptainstructor.all.each do |ptainstructa|
+#    ps.push(ptainstructa.id)
+#  end
+#  Teacher.all.each do |teacha|
+#    ts.push(teacha.id)
+#  end
+#  number_of_courses = 3
+#  number_of_courses.times do |t|
+#    randomGrade = grades[rand(grades.length)]
+#    randomMin = rand(50)
+#    randomMax = rand(50) + 50
+#    random = rand(100)
+#    ptainstructorId = ps[rand(ps.length)]
+#    teacherId = ts[rand(ts.length)]
+#    ptainstructor = Ptainstructor.find_by_id(ptainstructorId)
+#    teacher = Teacher.find_by_id(teacherId)
+#
+#    semester.courses.create(
+#      name: "#{my_semester} Course #{t+1}",
+#      description: "#{my_semester} course ##{t+1}",
+#      sunday: 1,
+#      wednesday: 1,
+#      start_time: "#{t+1}:00pm",
+#      end_time: "#{t+1}:00pm",
+#      grade_range: "#{randomGrade}",
+#      class_min: randomMin,
+#      class_max: randomMax,
+#      number_of_classes: randomMin,
+#      fee_per_meeting: random,
+#      fee_for_additional_materials: random,
+#      total_fee: random,
+#      ptainstructor: ptainstructor,
+#      teacher: teacher,
+#    )
+#  end
 end
 
 

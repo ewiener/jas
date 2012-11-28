44890f3f88c87cb483a8be84fa6bca46033a2014
TheUnknownUniverse <saasbook@saasbook.(none)>
2012-11-27 23:24:46 -0800

    Fixed the testing environment so that it now uses the test databse instead of our development database.

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,8 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
+ENV["RAILS_ENV"] = 'test'
+
 require 'simplecov'
 SimpleCov.start 'rails'
 require 'cucumber/rails'

c949b7b725c23d99cb7c7393c0e5863ac5545cb9
ewiener <ewiener@yahoo.com>
2014-09-09 14:58:32 -0700

    Remove reference to $.browser, no longer supported in jquery

--- vendor/assets/javascripts/jquery.print-preview.js
+++ vendor/assets/javascripts/jquery.print-preview.js
@@ -7,9 +7,9 @@
  *
  * Date: Wed Jan 25 00:00:00 2012 -000
  */
- 
-(function($) { 
-    
+
+(function($) {
+
 	// Initialization
 	$.fn.printPreview = function() {
 		this.each(function() {
@@ -22,14 +22,14 @@
 		});
 		return this;
 	};
-    
+
     // Private functions
     var mask, size, print_modal, print_controls;
     $.printPreview = {
         loadPrintPreview: function() {
             // Declare DOM objects
             print_modal = $('<div id="print-modal"></div>');
-            print_controls = $('<div id="print-modal-controls">' + 
+            print_controls = $('<div id="print-modal-controls">' +
                                     '<a href="#" class="print" title="Print page">Print page</a>' +
                                     '<a href="#" class="close" title="Close print preview">Close</a>').hide();
             var print_frame = $('<iframe id="print-modal-content" scrolling="no" border="0" frameborder="0" name="print-frame" />');
@@ -43,26 +43,26 @@
 
             // The frame lives
             for (var i=0; i < window.frames.length; i++) {
-                if (window.frames[i].name == "print-frame") {    
+                if (window.frames[i].name == "print-frame") {
                     var print_frame_ref = window.frames[i].document;
                     break;
                 }
             }
             print_frame_ref.open();
             print_frame_ref.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
-                '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">' + 
+                '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">' +
                 '<head><title>' + document.title + '</title></head>' +
                 '<body></body>' +
                 '</html>');
             print_frame_ref.close();
-            
+
             // Grab contents and apply stylesheet
             var $iframe_head = $('head link[media*=print], head link[media=all]').clone(),
                 $iframe_body = $('body > *:not(#print-modal):not(script)').clone();
             $iframe_head.each(function() {
                 $(this).attr('media', 'all');
             });
-            if (!$.browser.msie && !($.browser.version < 7) ) {
+            if (true /*!$.browser.msie && !($.browser.version < 7)*/ ) {
                 $('head', print_frame_ref).append($iframe_head);
                 $('body', print_frame_ref).append($iframe_body);
             }
@@ -74,12 +74,12 @@
                     $('head', print_frame_ref).append($(this).clone().attr('media', 'all')[0].outerHTML);
                 });
             }
-            
+
             // Disable all links
             $('a', print_frame_ref).bind('click.printPreview', function(e) {
                 e.preventDefault();
             });
-            
+
             // Introduce print styles
             $('head').append('<style type="text/css">' +
                 '@media print {' +
@@ -100,8 +100,8 @@
             $('img', print_frame_ref).load(function() {
                 print_frame.height($('body', print_frame.contents())[0].scrollHeight);
             });
-            
-            // Position modal            
+
+            // Position modal
             starting_position = $(window).height() + $(window).scrollTop();
             var css = {
                     top:         starting_position,
@@ -116,7 +116,7 @@
                     print_controls.fadeIn('slow').focus();
                 });
             print_frame.height($('body', print_frame.contents())[0].scrollHeight);
-            
+
             // Bind closure
             $('a', print_controls).bind('click', function(e) {
                 e.preventDefault();
@@ -124,7 +124,7 @@
                 else { $.printPreview.distroyPrintPreview(); }
             });
     	},
-    	
+
     	distroyPrintPreview: function() {
     	    print_controls.fadeOut(100);
     	    print_modal.animate({ top: $(window).scrollTop() - $(window).height(), opacity: 1}, 400, 'linear', function(){
@@ -133,57 +133,57 @@
     	    });
     	    mask.fadeOut('slow', function()  {
     			mask.remove();
-    		});				
+    		});
 
     		$(document).unbind("keydown.printPreview.mask");
     		mask.unbind("click.printPreview.mask");
     		$(window).unbind("resize.printPreview.mask");
 	    },
-	    
+
     	/* -- Mask Functions --*/
 	    loadMask: function() {
 	        size = $.printPreview.sizeUpMask();
             mask = $('<div id="print-modal-mask" />').appendTo($('body'));
-    	    mask.css({				
-    			position:           'absolute', 
-    			top:                0, 
+    	    mask.css({
+    			position:           'absolute',
+    			top:                0,
     			left:               0,
     			width:              size[0],
     			height:             size[1],
     			display:            'none',
-    			opacity:            0,					 		
+    			opacity:            0,
     			zIndex:             9999,
     			backgroundColor:    '#000'
     		});
-	
+
     		mask.css({display: 'block'}).fadeTo('400', 0.75);
-    		
+
             $(window).bind("resize..printPreview.mask", function() {
 				$.printPreview.updateMaskSize();
 			});
-			
+
 			mask.bind("click.printPreview.mask", function(e)  {
 				$.printPreview.distroyPrintPreview();
 			});
-			
+
 			$(document).bind("keydown.printPreview.mask", function(e) {
 			    if (e.keyCode == 27) {  $.printPreview.distroyPrintPreview(); }
 			});
         },
-    
+
         sizeUpMask: function() {
-            if ($.browser.msie) {
+            if (false /*$.browser.msie*/) {
             	// if there are no scrollbars then use window.height
             	var d = $(document).height(), w = $(window).height();
             	return [
             		window.innerWidth || 						// ie7+
-            		document.documentElement.clientWidth || 	// ie6  
+            		document.documentElement.clientWidth || 	// ie6
             		document.body.clientWidth, 					// ie6 quirks mode
             		d - w < 20 ? w : d
             	];
             } else { return [$(document).width(), $(document).height()]; }
         },
-    
+
         updateMaskSize: function() {
     		var size = $.printPreview.sizeUpMask();
     		mask.css({width: size[0], height: size[1]});

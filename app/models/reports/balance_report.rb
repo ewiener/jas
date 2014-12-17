4760afcde7636538f04cff371b87889384d77d5d
ewiener <ewiener@yahoo.com>
2014-12-17 00:34:16 -0800

    Add projected vs. actual expenses on balance report

--- app/models/reports/balance_report.rb
+++ app/models/reports/balance_report.rb
@@ -3,16 +3,17 @@ class BalanceReport < Report
 	attr_reader :total_student_fee, :total_student_class_fee, :total_student_registration_fee
 	attr_reader :total_student_registration_fee_waived
 
-	attr_reader :total_expenses
+	attr_reader :total_expenses_proj, :total_expenses_actual
 	attr_reader :total_student_refund, :total_student_class_refund, :total_student_registration_refund
 	attr_reader :total_instructor_fee
 	attr_reader :total_instructor_scholarship_amount
-	attr_reader :district_surcharge_amount
+	attr_reader :district_surcharge_amount_proj, :district_surcharge_amount_actual
 	attr_reader :district_surcharge_factor
 
 	attr_reader :total_scholarship_amount
+  attr_reader :total_instructor_invoiced_amount
 
-	attr_reader :balance
+	attr_reader :balance_proj, :balance_actual
 
 	def name
 		"Balance Report"
@@ -25,8 +26,9 @@ class BalanceReport < Report
 		@total_student_registration_fee_waived = @semester.registration_fees_waived || 0
 		@total_instructor_fee = 0
 		@total_instructor_scholarship_amount = 0
+    @total_instructor_invoiced_amount = 0
 		@total_scholarship_amount = 0
-		@balance = 0
+		@balance_proj = @balance_actual = 0
 
     processed_disenrolled_students = Hash.new
 
@@ -55,14 +57,21 @@ class BalanceReport < Report
         @total_instructor_scholarship_amount = @total_instructor_scholarship_amount + course.total_fee * course.instructor_scholarships.to_i
       end
     end
+    @semester.instructors.each do |instructor|
+      @total_instructor_invoiced_amount = @total_instructor_invoiced_amount + instructor.total_invoiced_amount.to_f
+    end
     @total_student_fee = @total_student_class_fee + @total_student_registration_fee
     @total_student_refund = @total_student_class_refund + @total_student_registration_refund
 
     @district_surcharge_factor = @semester.district_surcharge || 0
-    @district_surcharge_amount = (@total_instructor_fee - @total_instructor_scholarship_amount) * @district_surcharge_factor
+    @district_surcharge_amount_proj = (@total_instructor_fee - @total_instructor_scholarship_amount) * @district_surcharge_factor
 
     @total_revenue = @total_student_fee - @total_student_refund - @total_student_registration_fee_waived
-    @total_expenses = @total_instructor_fee - @total_instructor_scholarship_amount + @district_surcharge_amount
-    @balance = @total_revenue - @total_expenses
+    @total_expenses_proj = @total_instructor_fee - @total_instructor_scholarship_amount + @district_surcharge_amount_proj
+    @balance_proj = @total_revenue - @total_expenses_proj
+
+    @district_surcharge_amount_actual = @total_instructor_invoiced_amount * @district_surcharge_factor
+    @total_expenses_actual = @total_instructor_invoiced_amount + @district_surcharge_amount_actual
+    @balance_actual = @total_revenue - @total_expenses_actual
 	end
 end
